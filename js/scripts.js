632d3d7a74c7e82b0cf3364d3beb4690311d762f
Rafaela Lissa <paraaplicativos259@gmail.com>
2025-02-25 09:12:32 -0300

    ok

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 function seguir(){
     alert('Você agora está seguindo Rafaela Lissa')
 }
+//exercicio 1
+function exe1(){
+    //vamos usar DOM para recuperar os dados do html
+    //DOM - Document Object Model
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    //vamos fazer a subtração
+    let sub = nro1 - nro2
+    //vamos mostrar o resultado
+    //alert("A subtração é " + sub)
+    document.getElementById("sub").innerText = "o resultado é " + sub
+
+}
