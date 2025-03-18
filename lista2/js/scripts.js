55a626e999355bfa8b773bec9e48fdec63c06416
RAFAELA LISSA MACIEL DA SILVA <1090482421036@fatec-srv.local>
2025-03-18 09:08:33 -0300

    lista 2

--- lista2/js/scripts.js
+++ lista2/js/scripts.js
@@ -33,3 +33,63 @@ function exe2(){
     }
 
 }
+function exe3(){
+    let numero1 = Number(document.getElementById("numero1").value)
+    let numero2 = Number(document.getElementById("numero2").value)
+
+    let menor
+    if (numero1 < numero2){
+        document.getElementById("menor").innerText = "O menor é  " + numero1
+    }
+    else if (numero2 < numero1){
+        document.getElementById("menor").innerText = "O menor é  " + numero2
+    }
+    else {
+        document.getElementById("menor").innerText = "os dois são iguais "
+    }
+}
+function exe4(){
+    let num1 = Number(document.getElementById("num1").value)
+    let num2 = Number(document.getElementById("num2").value)
+    let num3 = Number(document.getElementById("num3").value)
+
+    let maior
+    if (num1 >= num2 && num1 >= num3){
+        document.getElementById("maior").innerText = "O maior é  " + num1
+    }
+    else if (num2 >= num1 && num2 >= num3) {
+        document.getElementById("maior").innerText = "O maior é  " + num2
+    }
+    else if (num3 >= num1 && num3 >= num2){
+        document.getElementById("maior").innerText = "O maior é  " + num3
+    }
+}
+function exe5(){
+    let = num1 = Number(document.getElementById("num1").value)
+    let = num2 = Number(document.getElementById("num2").value)
+    let selecao = Number(document.getElementById("selecao").value)
+    //utilização do escolha
+    switch (selecao){
+        case 1: document.getElementById("resultado").innerText = (num1 + num2)/2
+        break
+        
+        case 2: if (num1 >= num2){
+            document.getElementById("resultado").innerText = num1 - num2
+        }
+        else {
+            document.getElementById("resultado").innerText = num2 - num1
+        }
+        break
+
+        case 3: document.getElementById("resultado").innerText = num1 * num2
+        break
+        
+        case 4: if(num2 != 0){
+            document.getElementById("resultado").innerText = num1 / num2
+        }
+        else{
+            document.getElementById("resultado").innerText = "Divisão por zero"
+        }
+    }
+        
+}
